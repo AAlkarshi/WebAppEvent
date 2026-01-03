6ed14a16342eed936874577552aff6a246a95b20
Abdullrahman Al karshi <alkarshi.abdullrahman@gmail.com>
2026-01-03 19:53:05 +0100

    Mise en Place CI/CD Symfony - Tests & Déploiement

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -1,16 +1,32 @@
-<?php
-
+// src/DataFixtures/AppFixtures.php
 namespace App\DataFixtures;
 
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use App\Entity\Category;
+use App\Entity\Event;
 
 class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        // Créer des catégories
+        $categories = ['Musique', 'Sport', 'Conférence'];
+        foreach ($categories as $name) {
+            $category = new Category();
+            $category->setName($name);
+            $manager->persist($category);
+            $this->addReference($name, $category);
+        }
+
+        // Créer des événements
+        for ($i = 1; $i <= 5; $i++) {
+            $event = new Event();
+            $event->setTitle("Événement $i");
+            $event->setDescription("Description de l'événement $i");
+            $event->setCategory($this->getReference($categories[array_rand($categories)]));
+            $manager->persist($event);
+        }
 
         $manager->flush();
     }
